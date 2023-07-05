448930eb5eab6e6ada6a57dad4c1c976d511aad7
Ewelina <jrkewelina@gmail.com>
2023-07-05 18:46:22 +0200

    New algorithm inside Matrix::determinant() Pascal's triangle

--- Matrices_sum/Matrix.cpp
+++ Matrices_sum/Matrix.cpp
@@ -59,10 +59,33 @@ Matrix Matrix::transposeMatrix(Matrix& A) {
 
 //Determinant of matrix
 int Matrix::determinant(Matrix D) {
+
+	//Pascal's triangle
+	vector <int> vector2 = { 1,2,1 };
+	if (D.getDimentions().getNumberOfRows() > 2) {
+		vector <int> vector1{ 1 };
+
+		for (int i = 0; i < D.getDimentions().getNumberOfRows() - 2; i++) {
+
+			for (int j = 0; j < vector2.size() - 1; j++) {
+				vector1.push_back(vector2[j] + vector2[j + 1]);
+			}
+			vector1.push_back(1);
+			vector2 = vector1;
+			vector1.clear();
+			vector1.push_back(1);
+		}
+	}
+	for (int i = 0; i < vector2.size(); i++) {
+		cout << vector2[i] << " ";
+	}
+	cout << "\n";
+
 	int sum1 = 0;
 	int sum2 = 0;
 	int product = 1;
 	deque <int> dq;
+	vector <int> vec;
 
 	//new deck
 	for (int i = 0;i < D.getDimentions().getNumberOfRows();i++) {
@@ -72,12 +95,49 @@ int Matrix::determinant(Matrix D) {
 	//SUM
 	for (int j = 0;j < D.getDimentions().getNumberOfRows();j++) {
 		product = 1;
+
 		for (int i = 0;i < D.getDimentions().getNumberOfRows();i++) {
-			product *= D.getElements(i, dq[i]);
+			//if i==dq[i]
+			if (i == dq[i]) {			//polynomial
+
+				for (int w = 0; w < vector2.size(); w++) {
+
+					for (int p = 0;p < vector2[w];p++) {
+
+						product = 1;
+						for (int q = 0;q < D.getDimentions().getNumberOfRows() - w;q++) {
+							if (p + q >= D.getDimentions().getNumberOfRows() - 1 && q != D.getDimentions().getNumberOfRows() - w - 1) {
+								product *= D.getElements(q + 1, q + 1);
+							}
+							else {
+								product *= D.getElements(q, q);
+							}
+						}
+						sum1 += product;
+					}
+					vec.push_back(sum1);
+					product = 1;
+					sum1 = 0;
+				}
+			}
+			cin >> sum1;
+
+
+			//if i!=dq[i]
+		/*	if (i!=dq[i]) {
+				product *= D.getElements(i, dq[i]);
+
+			}*/
 		}
+
+		cout << endl;
+
+
 		int x = dq[0];
 		dq.pop_front();
 		dq.push_back(x);
+
+
 
 		sum1 += product;
 	}
@@ -101,4 +161,4 @@ int Matrix::determinant(Matrix D) {
 	}
 
 	return sum1 - sum2;
-}
+};
